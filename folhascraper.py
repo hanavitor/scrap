51a923f6db0207b2da582d5369ca1468ed65e120
hanavitor <vitorhanael@hotmail.com>
2019-03-18 00:25:38 -0300

    a

--- folhascraper.py
+++ folhascraper.py
@@ -1,14 +1,27 @@
 from requests_html import HTML, HTMLSession
+import re
 
 session = HTMLSession()
-r = session.get('https://www1.folha.uol.com.br/mundo/2019/03/homenageado-por-bannon-olavo-de-carvalho-diz-desconhecer-ideias-politicas-de-bolsonaro.shtml')
+r = session.get('https://www.folha.uol.com.br')
 
-news = r.html.find('div.c-news__body', first = True)
+hlinks = r.html.find('a.c-headline__url')
+slinks = r.html.find('a.c-list-links__url')
+hlinks.extend(slinks)
+links = []
 
-paragraphs = news.find('p')
+for link in hlinks:
+    links.append(link.attrs['href'])
 
-with open('file.out', 'w') as f:
-    for paragraph in paragraphs:
-        if paragraph.text == '':
-            continue
-        f.write(paragraph.text + "\n")
+for link in links:
+    if re.search("www1\.folha\.uol\.com\.br", link):
+        print(link)
+        rl = session.get(link)
+        news = rl.html.find('div.c-news__body', first = True)
+        
+        paragraphs = news.find('p')
+        
+        with open('file.out', 'a') as f:
+            for paragraph in paragraphs:
+                if paragraph.text == '':
+                    continue
+                f.write(paragraph.text + "\n")
