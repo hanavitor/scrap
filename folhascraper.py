4f76815bf2ca35b9bb043f440b0f8b583fe87ccf
hanavitor <vitorhanael@hotmail.com>
2019-03-18 23:51:03 -0300

    a

--- folhascraper.py
+++ folhascraper.py
@@ -1,5 +1,6 @@
 from requests_html import HTML, HTMLSession
 import re
+import os
 
 session = HTMLSession()
 r = session.get('https://www.folha.uol.com.br')
@@ -9,19 +10,34 @@ slinks = r.html.find('a.c-list-links__url')
 hlinks.extend(slinks)
 links = []
 
-for link in hlinks:
+for i,link in enumerate(hlinks):
     links.append(link.attrs['href'])
+    if i > 0 and links[-1] == links[-2]:
+        links.pop()
+
+if os.path.isfile('a.out'):
+        f = open('a.out', 'w')
+        f.close()
 
 for link in links:
-    if re.search("www1\.folha\.uol\.com\.br", link):
-        print(link)
-        rl = session.get(link)
-        news = rl.html.find('div.c-news__body', first = True)
-        
-        paragraphs = news.find('p')
-        
-        with open('file.out', 'a') as f:
-            for paragraph in paragraphs:
-                if paragraph.text == '':
-                    continue
-                f.write(paragraph.text + "\n")
+
+    print(link)
+    rl = session.get(link)
+    news = rl.html.find('div.c-news__body', first = True)
+
+    if isinstance(news,type(None)):
+        print("Page out of scope, see exceptions.out")
+        with open('exceptions.out', 'a') as f:
+            f.write(link + "\n")
+        continue
+
+    paragraphs = news.find('p')
+
+    with open('a.out', 'a') as f:
+        for paragraph in paragraphs:
+            if paragraph.text == '':
+                continue
+            #splits = paragraph.text.split('.')
+            #for split in splits:
+            #    f.write(split + "\n")
+            f.write(paragraph.text + "\n")
