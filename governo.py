273b8e8a15ee57792dde4bfda3d601b488ce1d77
hanavitor <vitorhanael@hotmail.com>
2019-03-29 13:10:20 -0300

    a

--- governo.py
+++ governo.py
@@ -1,41 +1,58 @@
 import requests_html as rh
+import re
 import sys
 import time
 import newspaper as nw
 import utils as u
+import visitedlinks as v
 
 session = rh.HTMLSession()
-
+vl = v.VistedLinks()
 flag = True
 links = []
 timeout = 2
 retry = 0
-i = 0
+i = 1
 
-while i<=390:
-    print("new")
-    r = session.get('http://paraiba.pb.gov.br/noticias?b_start:int='+str(i))
-    links = r.html.find('a.summary.url')
-    links = u.linkreturn(links)
+open(sys.argv[1],'w')
+
+while i<=203:
+    print("Page "+str(i)+" out of 203")
+    r = session.get('http://ctav.gov.br/category/noticias/page/'+str(i))
+    print(r.url)
+    rl = r.html.find('h2.entry-title')
+    for link in rl:
+        links.extend(link.find('a'))
+    links = u.link_return(links)
     for j,link in enumerate(links):
+        print(link)
+        print("Link " +str(j)+" of page " +str(i))
+        if vl.repeated_links(link):
+            print("Link already scraped")
+            continue
+        vl.add_link(link)
         article = nw.Article(link)
-        while True:
-            print(j)
-            print(link)
-            try:
-                time.sleep(timeout)
-                article.download()
-                article.parse()
-            except nw.article.ArticleException:
-                print("erro")
-                timout = timeout+1
-                retry = retry+1
-                if retry == 3:
-                    retry = 0
-                    timeout = 2
-                    break
-                continue
-            break
+        article.download()
+        article.parse()
+        #while True:
+        #    print(j)
+        #    print(link)
+        #    try:
+        #        time.sleep(timeout)
+        #        article.download()
+        #        article.parse()
+        #    except nw.article.ArticleException:
+        #        print("erro")
+        #        timout = timeout+1
+        #        retry = retry+1
+        #        if retry == 3:
+        #            retry = 0
+        #            timeout = 2
+        #            break
+        #        continue
+        #    break
         with open(sys.argv[1],'a') as f:
-            f.write(article.text)
-    i = i+30
+            text = re.sub("\\n\\n","\\n",article.text)
+            f.write(text+"\n")
+    links.clear()
+    i = i+1
--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ def link_return(hlinks):
     for i,link in enumerate(hlinks):
         #adiciona links encontrados no atributo href
         links.append(link.attrs['href'])
-        if re.match("http",link[i]) == False:
+        if re.match("http",link.attrs['href']) == False:
             #remove hrefs que nao sao links
             link.pop()
         if i > 0 and links[-1] == links[-2]:
